dd3c4860a0bb09afcc418e36d9febfc51ca09642
Phil Kunz <gitlab@philkunz.com>
2019-05-15 21:06:07 +0200

    fix(core): update

--- ts/tsdoc.classes.mkdocs.ts
+++ ts/tsdoc.classes.mkdocs.ts
@@ -1,10 +1,32 @@
 import * as plugins from './tsdoc.plugins';
+import * as paths from './tsdoc.paths';
 
 export class MkDocs {
+  public smartshellInstance = new plugins.smartshell.Smartshell({
+    executor: 'bash',
+    pathDirectories: [paths.binDir]
+  });
+
   public static async isMkDocsDir(dirPathArg: string): Promise<boolean> {
     const result = await plugins.smartfile.fs.fileExists(
       plugins.path.join(dirPathArg, 'mkdocs.yml')
     );
-    return !result;
+    return result;
+  }
+
+  // Instance
+  public typedocDirectory: string;
+  constructor(dirPathArg) {
+    this.typedocDirectory = dirPathArg; 
+  }
+
+  public async compile () {
+    await this.smartshellInstance.exec(`rm -rf public/`);
+    await this.smartshellInstance.exec(`docker run --rm -it -p 8000:8000 -v ${paths.cwd}:/docs mkdocs build`);
+    await this.smartshellInstance.exec(`gitzone commit`);
+  }
+
+  public async serve () {
+    await this.smartshellInstance.exec(`docker run --rm -it -p 8000:8000 -v ${paths.cwd}:/docs registry.gitlab.com/hosttoday/ht-docker-mkdocs`);
   }
 }
